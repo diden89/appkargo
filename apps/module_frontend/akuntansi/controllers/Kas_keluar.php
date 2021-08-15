80cd9a1f3c5dd12b84f6fab8368bc7ad62ad27ed
Dian Kosasih <algazasoution@gmail.com>
2021-08-16 06:53:53 +0700

    up

---
+++ apps/module_frontend/akuntansi/controllers/Kas_keluar.php
@@ -0,0 +1,399 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*!
+ * @package APPKARGO
+ * @copyright Noobscript
+ * @author Sikelopes
+ * @edit Diden89
+ * @version 1.0
+ * @access Public
+ * @path /appkargo/apps/module_frontend/akuntansi/controllers/Kas_Keluar.php
+ */
+
+class Kas_Keluar extends NOOBS_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('akuntansi/kas_keluar_model', 'db_cash_out');
+	}
+
+	public function index()
+	{
+		$this->store_params['header_title'] = 'Daftar Kas Keluar';
+		$this->store_params['breadcrumb'] = array(
+			array('', 'Home'),
+			array('akuntansi/kas_keluar', 'Kas Keluar')
+		);
+
+		$this->store_params['source_bot'] = array(
+			'<script src="'.base_url('vendors/jquery-number-master/jquery.number.js').'"></script>',
+			'<script src="'.base_url('scripts/akuntansi/kas_keluar.js').'"></script>',
+		);
+
+		$this->store_params['item'] = [];
+		$date = date('d-m-Y H:i:s');
+		$date = strtotime($date);
+		$date = strtotime("-7 day", $date);
+		// echo date('d-m-Y H:i:s', $date);
+
+		$params['date_range1'] = date('Y-m-d H:i:s', $date);
+		$params['date_range2'] = date('Y-m-d H:i:s');
+		// print_r($params);exit;
+		$load_data_kas_keluar = $this->db_cash_out->load_data_kas_keluar($params);
+
+		if ($load_data_kas_keluar->num_rows() > 0)
+		{
+			$num = 0;
+			$result = $load_data_kas_keluar->result();
+
+			foreach ($result as $k => $v)
+			{
+				$num++;
+
+				$get_progress_so = $this->db_cash_out->get_progress_so(array('so_id' => $v->so_id,'dod_is_status' => 'SELESAI'));
+
+				$get_total_so = $this->db_cash_out->get_progress_so(array('so_id' => $v->so_id));
+
+				if($get_progress_so->num_rows() > 0) {
+					$progress = $get_progress_so->row();
+					$v->progress =  $progress->progress;
+				}
+				else
+				{
+					$v->progress = 0;
+				}
+
+				if($get_total_so->num_rows() > 0) {
+					$total = $get_total_so->row();
+					$v->total =  $total->progress;
+				}
+				else
+				{
+					$v->total = 0;
+				}
+				
+				$v->num = $num;
+			}
+			// print_r($result);exit;
+
+			$this->store_params['item'] = $result;
+		}
+		$this->store_params['date_range1'] = date('Y-m-d H:i:s', $date);
+
+		$this->view('kas_keluar_view');
+	}
+
+	public function load_kas_keluar_form()
+	{
+		if (isset($_POST['action']) && $_POST['action'] == 'load_kas_keluar_form')
+		{
+			$post = $this->input->post(NULL, TRUE);
+			$params = array(
+				'table' => 'province'
+			);
+
+			$post['province'] = $this->db_cash_out->get_option_province()->result();
+			$post['vendor'] = $this->db_cash_out->get_option_vendor()->result();
+			$post['item_list'] = $this->db_cash_out->get_option_item_list()->result();
+			$get_last_notrx = $this->db_cash_out->get_last_notrx();
+
+			if($get_last_notrx->num_rows() > 0)
+			{
+				$notrx = $get_last_notrx->row();
+				$last_notrx = $notrx->notrx + 1;
+			}
+			else
+			{
+				$last_notrx = 1;
+			}
+
+			$post['last_notrx'] = sprintf('%04d',$last_notrx);
+			
+
+			if($post['mode'] == 'edit')
+			{
+				$post['data'] = $this->db_cash_out->load_data_kas_keluar($post)->row();
+			}
+	
+			$this->_view('kas_keluar_form_view', $post);
+		}
+		else $this->show_404();
+	}
+
+	public function get_item_list_option()
+	{
+		$post = $this->input->post(NULL, TRUE);
+
+
+		if (isset($post['action']) && ! empty($post['action']) && $post['action'] == 'get_item_list_option')
+		{
+			unset($post['action']);
+
+			$get_item_list_option = $this->db_cash_out->get_item_list_option($post);
+
+			if ($get_item_list_option->num_rows() > 0) 
+			{
+				$result = $get_item_list_option->result();
+
+				echo json_encode(array('success' => TRUE, 'data' => $result));
+			}
+			else echo json_encode(array('success' => FALSE, 'msg' => 'Data Not Found!'));
+		}
+		else $this->show_404();
+	}
+
+	public function get_region_option()
+	{
+		$post = $this->input->post(NULL, TRUE);
+
+
+		if (isset($post['action']) && ! empty($post['action']) && $post['action'] == 'get_region_option')
+		{
+			unset($post['action']);
+
+			$get_region_option = $this->db_cash_out->get_region_option($post);
+
+			if ($get_region_option->num_rows() > 0) 
+			{
+				$result = $get_region_option->result();
+
+				echo json_encode(array('success' => TRUE, 'data' => $result));
+			}
+			else echo json_encode(array('success' => FALSE, 'msg' => 'Data Not Found!'));
+		}
+		else $this->show_404();
+	}
+	public function get_district_option()
+	{
+		$post = $this->input->post(NULL, TRUE);
+
+
+		if (isset($post['action']) && ! empty($post['action']) && $post['action'] == 'get_district_option')
+		{
+			unset($post['action']);
+
+			$get_district_option = $this->db_cash_out->get_district_option($post);
+
+			if ($get_district_option->num_rows() > 0) 
+			{
+				$result = $get_district_option->result();
+
+				echo json_encode(array('success' => TRUE, 'data' => $result));
+			}
+			else echo json_encode(array('success' => FALSE, 'msg' => 'Data Not Found!'));
+		}
+		else $this->show_404();
+	}
+
+	public function load_data_kas_keluar()
+	{
+		if (isset($_POST['action']) && $_POST['action'] == 'load_data_kas_keluar')
+		{
+			$post = $this->input->post(NULL, TRUE);
+			$load_data_kas_keluar = $this->db_cash_out->load_data_kas_keluar($post);
+			// print_r($_POST);exit;
+			if ($load_data_kas_keluar->num_rows() > 0) 
+			{
+				$result = $load_data_kas_keluar->result();
+				$number = 1;
+
+				foreach ($result as $k => $v)
+				{
+					$v->no = $number;
+
+					$number++;
+				}
+
+				echo json_encode(array('success' => TRUE, 'data' => $result));
+			}
+			else echo json_encode(array('success' => FALSE, 'msg' => 'Data not found!'));
+		}
+		else $this->show_404();
+	}
+
+	public function load_data_temporary_detail_so()
+	{
+		if (isset($_POST['action']) && $_POST['action'] == 'load_data_temporary_detail_so')
+		{
+			// print_r($_POST);exit;
+			$post = $this->input->post(NULL, TRUE);
+			$load_data_detail_so = $this->db_cash_out->load_data_detail_so($post);
+			if ($load_data_detail_so->num_rows() > 0) 
+			{
+				$result = $load_data_detail_so->result();
+				$number = 1;
+
+				foreach ($result as $k => $v)
+				{
+					$v->no = $number;
+
+					$number++;
+				}
+				
+				echo json_encode(array('success' => TRUE, 'data' => $result));
+			}
+			else echo json_encode(array('success' => FALSE, 'msg' => 'Data not found!'));
+		}
+		else $this->show_404();
+	}
+
+	public function store_data_kas_keluar()
+	{
+		// print_r($_POST);exit;
+		if (isset($_POST['action']) && $_POST['action'] == 'store_data_kas_keluar')
+		{
+			$post = $this->input->post(NULL, TRUE);
+			$store_data_kas_keluar = $this->db_cash_out->store_data_kas_keluar($post);
+
+			if ($store_data_kas_keluar->num_rows() > 0) 
+			{
+				$result = $store_data_kas_keluar->result();
+				$number = 1;
+
+				foreach ($result as $k => $v)
+				{
+					$v->no = $number;
+
+					$number++;
+				}
+
+
+				echo json_encode(array('success' => TRUE, 'data' => $result));
+			}
+			else echo json_encode(array('success' => FALSE, 'msg' => 'Data not found!'));
+		}
+		else $this->show_404();
+	}
+
+	public function store_data_temporary()
+	{
+		if (isset($_POST['action']) && $_POST['action'] == 'insert_temporary_data')
+		{
+			$post = $this->input->post(NULL, TRUE);
+			// print_r($post);exit;
+			$store_detail_so = $this->db_cash_out->store_detail_so($post);
+
+			if ($store_detail_so->num_rows() > 0) 
+			{
+				$result = $store_detail_so->result();
+				$number = 1;
+
+				foreach ($result as $k => $v)
+				{
+					$v->no = $number;
+
+					$number++;
+				}
+				
+				echo json_encode(array('success' => TRUE, 'data' => $result));
+			}
+			else echo json_encode(array('success' => FALSE, 'msg' => 'Data not found!'));
+		}
+		else $this->show_404();
+	}
+	
+	// public function store_data_temporary()
+	// {
+	// 	if (isset($_POST['action']) && $_POST['action'] == 'insert_temporary_data')
+	// 	{
+	// 		$post = $this->input->post(NULL, TRUE);
+
+	// 		$get_data_item = $this->db_cash_out->get_option_item_list($post)->row();
+			
+	// 		$idx = (! empty($this->session->userdata('temp_data'))) ? (count($this->session->userdata('temp_data')) - 1) +1 : 0;
+			
+	// 		if($idx == 0)
+	// 		{
+	// 			$temp[$idx] = (object) array(
+	// 				'qty' => $post['qty'],
+	// 				'il_item_name' => $get_data_item->il_item_name
+	// 			);				
+			
+	// 			$this->session->set_userdata(array(
+	// 				'temp_data' => $temp
+	// 			));
+	// 		}
+	// 		else
+	// 		{				
+	// 			$temp = (object) array(
+	// 				'qty' => $post['qty'],
+	// 				'il_item_name' => $get_data_item->il_item_name
+	// 			);
+
+	// 			array_push($_SESSION['temp_data'],$temp);
+				
+	// 		} 
+
+			
+	// 		// print_r($this->session);exit;
+	// 		if (count($this->session->userdata('temp_data')) > 0) 
+	// 		{
+	// 			$idx = 0;
+	// 			$temporary = $this->session->userdata('temp_data');
+	// 			foreach ($temporary as $k => $v)
+	// 			{
+	// 				$v->idx = $idx;
+
+	// 				$idx++;
+	// 			}
+				
+	// 			echo json_encode(array('success' => TRUE, 'data' => $temporary));
+	// 		}
+	// 		else echo json_encode(array('success' => FALSE, 'msg' => 'Data not found!'));
+	// 	}
+	// 	else $this->show_404();
+	// }
+
+	public function delete_data_item()
+	{
+		if (isset($_POST['action']) && $_POST['action'] == 'delete_data_item')
+		{
+			$post = $this->input->post(NULL, TRUE);
+			$delete_data_item = $this->db_cash_out->delete_data_item($post);
+
+			if ($delete_data_item->num_rows() > 0) 
+			{
+				$result = $delete_data_item->result();
+				$number = 1;
+
+				foreach ($result as $k => $v)
+				{
+					$v->no = $number;
+
+					$number++;
+				}
+				
+				echo json_encode(array('success' => TRUE, 'data' => $result));
+			}
+			else echo json_encode(array('success' => FALSE, 'msg' => 'Data not found!'));
+		}
+		else $this->show_404();
+	}
+
+	public function delete_data_temp()
+	{
+		// print_r($_POST);exit;
+		if (isset($_POST['action']) && $_POST['action'] == 'delete_data_temp')
+		{
+			$post = $this->input->post(NULL, TRUE);
+			$delete_data_sod = $this->db_cash_out->delete_data_so_detail($post);
+
+			if ($delete_data_sod->num_rows() > 0) 
+			{
+				$result = $delete_data_sod->result();
+				$number = 1;
+
+				foreach ($result as $k => $v)
+				{
+					$v->no = $number;
+
+					$number++;
+				}
+				
+				echo json_encode(array('success' => TRUE, 'data' => $result));
+			}
+			else echo json_encode(array('success' => FALSE, 'msg' => 'Data not found!','data' => array()));
+		}
+		else $this->show_404();
+	}
+}
---
+++ scripts/akuntansi/kas_keluar.js
@@ -0,0 +1,621 @@
+/*!
+ * @package APPKARGO
+ * @copyright Noobscript
+ * @author Sikelopes
+ * @edit Diden89
+ * @version 1.0
+ * @access Public
+ * @path /rab_frontend/scripts/akuntansi/kas_keluar.js
+ */
+
+const daftarCashOutList = {
+	selectedData: '',
+	init: function() {
+		const me = this;
+
+		$('#btnSearchItem').click(function(e) {
+			e.preventDefault();
+			me.loadDataItem(this);
+		});
+		$('#btnReloadItem').click(function(e) {
+			$('#txtList').val('')
+			e.preventDefault();
+			me.loadDataItem(this);
+		});
+
+		$('#txtList').keydown(function(e) {
+			const keyCode = (e.keyCode ? e.keyCode : e.which);
+
+			if (keyCode == 13) {
+				$('#btnSearchItem').trigger('click');
+			}
+		});
+
+		$('#btnAddItem').click(function(e) {
+			e.preventDefault();
+			me.showItem(this,'add');
+		});
+
+		$('#range1').inputmask('dd-mm-yyyy', { 'placeholder': 'DD-MM-YYYY' });
+		$('#range1').noobsdaterangepicker({
+			// parentEl: "#" + popup[0].id + " .modal-body",
+			showDropdowns: true,
+			singleDatePicker: true,
+			locale: {
+				format: 'DD-MM-YYYY'
+			}
+		});
+		$('#range2').inputmask('dd-mm-yyyy', { 'placeholder': 'DD-MM-YYYY' });
+		$('#range2').noobsdaterangepicker({
+			// parentEl: "#" + popup[0].id + " .modal-body",
+			showDropdowns: true,
+			singleDatePicker: true,
+			locale: {
+				format: 'DD-MM-YYYY'
+			}
+		});
+	},
+	loadDataItem: function(el) {
+		const me = this;
+		const $this = $(el);
+
+		$.ajax({
+			url: siteUrl('akuntansi/kas_keluar/load_data_kas_keluar'),
+			type: 'POST',
+			dataType: 'JSON',
+			data: {
+				action: 'load_data_kas_keluar',
+				txt_item: $('#txtList').val()
+			},
+			success: function(result) {
+				$('#ignoredItemDataTable tbody').html('');
+
+				if (result.success !== false) me._generateItemDataTable(result.data);
+				else if (typeof(result.msg) !== 'undefined') toastr.error(result.msg);
+				else toastr.error(msgErr);
+			},
+			error: function(error) {
+				toastr.error(msgErr);
+			}
+		});
+	},
+	loadDataItemTemporary: function(el) {
+		const me = this;
+		const $this = $(el);
+		
+		$.ajax({
+			url: siteUrl('akuntansi/kas_keluar/load_data_temporary_detail_so'),
+			type: 'POST',
+			dataType: 'JSON',
+			data: {
+				action: 'load_data_temporary_detail_so',
+				txt_item: $('#txtList').val(),
+				no_trx:$('#no_trx_id').val()
+			},
+			success: function(result) {
+				$('#temporaryDataTable tbody').html('');
+
+				if (result.success !== false) me._generateTemporaryDataTable(result.data);
+				else if (typeof(result.msg) !== 'undefined') toastr.error(result.msg);
+				else toastr.error(msgErr);
+			},
+			error: function(error) {
+				toastr.error(msgErr);
+			}
+		});
+	},
+	_generateItemDataTable: (data) => {
+		const $this = $('#ignoredItemDataTable tbody');
+
+		$this.html('');
+
+		let body = '';
+
+		$.each(data, (idx, item) => {
+			body += '<tr>';
+			body += '<td>' + item.no + '</td>';
+			body += '<td>' + item.so_no_trx + '</td>';
+			body += '<td>' + item.v_vendor_name + '</td>';
+			body += '<td>' + item.so_qty + '</td>';
+			body += '<td>' + item.rd_name + '</td>';
+			body += '<td>' + item.date_create + '</td>';
+			body += '<td>' + item.so_is_status + '</td>';
+			body += '<td>' + item.paying + '</td>';
+			body += '<td>';
+				body += '<div class="btn-group btn-group-sm" role="group" aria-label="Action Button">';
+					body += '<button type="button" class="btn btn-success" data-id="' + item.so_id + '" data-no_trx="' + item.so_no_trx + '" data-rd_id="' + item.rd_id + '" data-rp_id="' + item.rd_province_id + '" onclick="daftarCashOutList.showItem(this, \'edit\');"><i class="fas fa-edit"></i></button>';
+					body += '<button type="button" class="btn btn-danger" data-id="' + item.so_id + '"  data-no_trx="' + item.so_no_trx + '" onclick="daftarCashOutList.deleteDataItem(this);"><i class="fas fa-trash-alt"></i></button>';
+				body += '</div>';
+			body += '</td>';
+			body += '</tr>';
+		});
+
+		$this.html(body);
+	},
+	_generateTemporaryDataTable: (data) => {
+		const $this = $('#temporaryDataTable tbody');
+
+		$this.html('');
+
+		let body = '';
+
+		$.each(data, (idx, item) => {
+			body += '<tr>';
+			// body += '<td>' + item.no + '</td>';
+			body += '<td>' + item.il_item_name + '</td>';
+			body += '<td>' + item.sod_qty + '</td>';
+			body += '<td>';
+				body += '<div class="btn-group btn-group-sm" role="group" aria-label="Action Button">';
+					body += '<button type="button" class="btn btn-success" data-id="' + item.sod_id + '" data-no_trx="' + item.so_no_trx + '" data-il_id="' + item.il_id + '" data-v_id="' + item.so_vendor_id + '" data-sod_qty="' + item.sod_qty + '" onclick="daftarCashOutList.editDetailSO(this, \'edit\');"><i class="fas fa-edit"></i></button>';
+					body += '<button type="button" class="btn btn-danger" data-id="' + item.sod_id + '" data-no_trx="' + item.sod_no_trx + '" onclick="daftarCashOutList.deleteDataTemp(this);"><i class="fas fa-trash-alt"></i></button>';
+				body += '</div>';
+			body += '</td>';
+			body += '</tr>';
+		});
+
+		$this.html(body);
+	},
+	editDetailSO: function(el) {
+		const me = this;
+		id = $(el).data('id');
+		v_id = $(el).data('v_id');
+		il_id = $(el).data('il_id');
+		sod_qty = $(el).data('sod_qty');
+
+		me.generateItemList(v_id,il_id);
+		$('#sod_qty').val(sod_qty);
+		$('#sod_id').val(id);
+	},
+	showItem: function(el, mode) {
+		console.log(mode)
+		const me = this;
+		let params = {action: 'load_kas_keluar_form'};
+	
+
+		if (mode == 'edit') {
+			params.mode = mode;
+			title = 'Edit';
+			params.txt_item = $(el).data('item');
+			params.txt_id = $(el).data('id');
+			params.rd_id = $(el).data('rd_id');
+			params.rsd_id = $(el).data('rsd_id');
+		}
+		else
+		{
+			params.mode = 'add';
+			title = 'Add';
+		}
+
+		$('#txt_province').on('select',function(){
+		});
+
+		$.popup({
+			title: title + ' Sales Order',
+			id: 'showItem',
+			size: 'large',
+			proxy: {
+				url: siteUrl('akuntansi/kas_keluar/load_kas_keluar_form'),
+				params: params
+			},
+			buttons: [{
+				btnId: 'saveData',
+				btnText:'Save',
+				btnClass: 'info',
+				btnIcon: 'far fa-check-circle',
+				onclick: function(popup) {
+					const form  = popup.find('form');
+
+					if ($.validation(form)) {
+						const formData = new FormData(form[0]);
+
+						$.ajax({
+							url: siteUrl('akuntansi/kas_keluar/store_data_kas_keluar'),
+							type: 'POST',
+							dataType: 'JSON',
+							data: formData,
+							processData: false,
+							contentType: false,
+	         				cache: false,
+							success: function(result) {
+								if (result.success) {
+									toastr.success(msgSaveOk);
+									me._generateItemDataTable(result.data);
+								} else if (typeof(result.msg) !== 'undefined') toastr.error(result.msg);
+								else toastr.error(msgErr);
+
+								popup.close();
+
+							},
+							error: function(error) {
+								toastr.error(msgErr);
+							}
+						});
+					}
+				}
+			}, {
+				btnId: 'closePopup',
+				btnText:'Close',
+				btnClass: 'secondary',
+				btnIcon: 'fas fa-times',
+				onclick: function(popup) {
+					popup.close();
+				}
+			}],
+			listeners: {
+				onshow: function(popup) {
+					if (mode == 'edit') {
+						daftarCashOutList.generateRegion($('#txt_province').val(),$(el).data('rd_id'));
+						daftarCashOutList.loadDataItemTemporary();
+					}
+
+					if (typeof(mode) !== 'undefined') {
+						mode = mode;
+					}
+					else
+					{
+						mode = 'add';
+					}
+
+					var today = new Date();
+					var dd = String(today.getDate()).padStart(2, '0');
+					var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+					var yyyy = today.getFullYear();
+
+					today = yyyy+mm+dd;
+					
+					// $('#so_no_trx').val($('#last_notrx').val());
+
+					$('#btnAddDetail').click(function(){
+						var qty = $('#sod_qty').val();
+							il_id = $('#il_id').val();
+							no_trx = $('#no_trx_id').val();
+							so_id = $('#sod_id').val();
+
+							
+							if(no_trx == "")
+							{
+								Swal.fire({
+									title: 'No Transaksi Tidak Ditemukan',
+									text: "Nomor Transaksi Tidak Ditemukan, input Nomor Transaksi Terlebih Dahulu!",
+									type: 'warning',
+									showCancelButton: false,
+									confirmButtonColor: '#17a2b8',
+									cancelButtonColor: '#d33',
+									confirmButtonText: 'Close!'
+								})
+							}
+							else
+							{
+								$.ajax({
+									url: siteUrl('akuntansi/kas_keluar/store_data_temporary'),
+									type: 'POST',
+									dataType: 'JSON',
+									data: {
+										action: 'insert_temporary_data',
+										sod_qty: qty,
+										sod_item_id: il_id,
+										sod_no_trx: no_trx,
+										so_id: so_id,
+										mode : mode
+									},
+									success: function(result) {
+										if (result.success) {
+											toastr.success("Data succesfully added.");
+											daftarCashOutList._generateTemporaryDataTable(result.data);
+
+										} else if (typeof(result.msg) !== 'undefined') {
+											toastr.error(result.msg);
+										} else {
+											toastr.error(msgErr);
+										}
+										
+									},
+									error: function(error) {
+										toastr.error(msgErr);
+									}
+								});								
+							}
+					});
+					$('#created_date').inputmask('dd-mm-yyyy', { 'placeholder': 'DD-MM-YYYY' });
+						$('#created_date').noobsdaterangepicker({
+							parentEl: "#" + popup[0].id + " .modal-body",
+							showDropdowns: true,
+							singleDatePicker: true,
+							locale: {
+								format: 'DD-MM-YYYY'
+							}
+						});
+					$('#v_vendor_id').change(function() {
+						var me = $(this);
+
+						if (me.val() !== '') {
+							
+							daftarCashOutList.generateItemList(me.val());
+
+						} else {
+							$('#il_id').html($('<option>', {
+								value: '',
+								text: 'Item Barang'
+							}));
+
+							$('#il_id').attr('disabled', true);
+						}
+					});
+					$('#il_id').change(function() {
+						
+						$('#sod_qty').val('');
+						$('#sod_id').val('');
+						
+					});
+					$('#txt_province').change(function() {
+						var me = $(this);
+						// console.log(me.val())
+
+							if (me.val() !== '') {
+								
+								daftarCashOutList.generateRegion(me.val());
+
+							} else {
+								$('#txt_region').html($('<option>', {
+									value: '',
+									text: 'Pilih Provinsi'
+								}));
+
+								$('#txt_region').attr('disabled', true);
+							}
+							$('#txt_district').attr('disabled', true);
+							$('#txt_district').html($('<option>', {
+								value: '',
+								text: '--Pilih Kabupaten / Kota--'
+							}));
+					});	
+
+					$('#txt_region').change(function() {
+						var me = $(this);
+						// console.log(me)
+
+							if (me.val() !== '') {
+								
+								daftarCashOutList.generateDistrict(me.val());
+
+							} else {
+								$('#txt_district').html($('<option>', {
+									value: '',
+									text: 'Pilih Kabupaten/Kota'
+								}));
+
+								$('#txt_district').attr('disabled', true);
+							}
+					});
+
+					// console.log(mode)				
+				}
+			}
+		});
+
+	},
+	generateItemList: function(vendor_id, il_id = false) {
+		var itemList = $('#il_id');
+			// console.log(provinceId)
+			$.ajax({
+				url: siteUrl('akuntansi/kas_keluar/get_item_list_option'),
+				type: 'POST',
+				dataType: 'JSON',
+				beforeSend: function() {},
+				complete: function() {},
+				data: {
+					action: 'get_item_list_option',
+					vendor_id: vendor_id
+				},
+				success: function (result) {
+					if (result.success) {
+						var data = result.data;
+
+						itemList.attr('disabled', false);
+
+						itemList.html($('<option>', {
+							value: '',
+							text: '--Pilih Item--'
+						}));
+						
+						data.forEach(function (newData) {
+							itemList.append($('<option>', {
+								value: newData.il_id,
+								text: newData.il_item_name
+							}));
+						});
+
+						if (il_id !== false) itemList.val(il_id);
+
+					} else {
+
+						itemList.html($('<option>', {
+							value: '',
+							text: 'Item Barang Tidak Ditemukan!'
+						}));
+					}
+				},
+				error: function (error) {
+					toastr.error(msgErr);
+				}
+			});
+	},
+	generateRegion: function(provinceId, regionId = false) {
+		var region = $('#txt_region');
+			console.log(regionId)
+			$.ajax({
+				url: siteUrl('akuntansi/kas_keluar/get_region_option'),
+				type: 'POST',
+				dataType: 'JSON',
+				beforeSend: function() {},
+				complete: function() {},
+				data: {
+					action: 'get_region_option',
+					prov_id: provinceId
+				},
+				success: function (result) {
+					if (result.success) {
+						var data = result.data;
+
+						region.attr('disabled', false);
+
+						region.html($('<option>', {
+							value: '',
+							text: '--Pilih Kabupaten / Kota--'
+						}));
+						
+						data.forEach(function (newData) {
+							region.append($('<option>', {
+								value: newData.rd_id,
+								text: newData.rd_name
+							}));
+						});
+
+						if (regionId !== false) region.val(regionId);
+
+					} else {
+
+						region.html($('<option>', {
+							value: '',
+							text: 'Kabupaten tidak ditemukan!'
+						}));
+					}
+				},
+				error: function (error) {
+					toastr.error(msgErr);
+				}
+			});
+	},
+	generateDistrict: function(regionId, districtId = false) {
+		var district = $('#txt_district');
+			
+			$.ajax({
+				url: siteUrl('akuntansi/kas_keluar/get_district_option'),
+				type: 'POST',
+				dataType: 'JSON',
+				beforeSend: function() {},
+				complete: function() {},
+				data: {
+					action: 'get_district_option',
+					district_id: regionId
+				},
+				success: function (result) {
+					if (result.success) {
+						var data = result.data;
+
+						district.attr('disabled', false);
+
+						district.html($('<option>', {
+							value: '',
+							text: '--Pilih Kecamatan--'
+						}));
+						
+						data.forEach(function (newData) {
+							district.append($('<option>', {
+								value: newData.rsd_id,
+								text: newData.rsd_name
+							}));
+						});
+
+						if (districtId !== false) district.val(districtId);
+
+					} else {
+
+						district.html($('<option>', {
+							value: '',
+							text: 'Kecamatan tidak ditemukan!'
+						}));
+					}
+				},
+				error: function (error) {
+					toastr.error(msgErr);
+				}
+			});
+	},
+	deleteDataItem: function(el) {
+		const me = this;
+		const $this = $(el);
+
+		Swal.fire({
+			title: 'Are you sure?',
+			text: "Data that has been deleted cannot be restored!",
+			type: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#17a2b8',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Yes, delete this data!'
+		}).then((result) => {
+			if (result.value) {
+				$.ajax({
+					url: siteUrl('akuntansi/kas_keluar/delete_data_item'),
+					type: 'POST',
+					dataType: 'JSON',
+					data: {
+						action: 'delete_data_item',
+						txt_id: $this.data('id')
+					},
+					success: function(result) {
+						$('#ignoredItemDataTable tbody').html('');
+						
+						if (result.success) {
+							 me._generateItemDataTable(result.data);
+						}						
+						else if (typeof(result.msg) !== 'undefined') {						
+							toastr.error(result.msg);
+						}
+						else {							
+							toastr.error(msgErr);
+						}
+
+					},
+					error: function(error) {
+						toastr.error(msgErr);
+					}
+				});
+			}
+		});
+	},
+	deleteDataTemp: function(el) {
+		const me = this;
+		const $this = $(el);
+			
+		$.ajax({
+			url: siteUrl('akuntansi/kas_keluar/delete_data_temp'),
+			type: 'POST',
+			dataType: 'JSON',
+			data: {
+				action: 'delete_data_temp',
+				id: $this.data('id'),
+				sod_no_trx: $this.data('no_trx')
+			},
+			success: function(result) {
+				$('#temporaryDataTable tbody').html('');
+
+				if (result.success) {
+					 daftarCashOutList._generateTemporaryDataTable(result.data);
+				}
+				else if (result.success == false)
+				{
+					daftarCashOutList._generateTemporaryDataTable(result.data);
+				}
+				else if (typeof(result.msg) !== 'undefined') {
+					$('#temporaryDataTable tbody').html('');
+					toastr.error(result.msg);
+				}
+				else {
+					$('#temporaryDataTable tbody').html('');
+					toastr.error(msgErr);
+				}
+
+			},
+			error: function(error) {
+				toastr.error(msgErr);
+			}
+		});
+	}
+	
+};
+
+$(document).ready(function() {
+	daftarCashOutList.init();
+});
