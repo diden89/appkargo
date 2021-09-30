0fdfb71278b91ad21b2b32161833bbe986ce7f7f
Dian Kosasih <algazasolution@gmail.com>
2021-09-30 15:57:23 +0700

    up

---
+++ scripts/report/report_delivery_order.js
@@ -0,0 +1,41 @@
+/*!
+ * @package APPKARGO
+ * @copyright Noobscript
+ * @author Sikelopes
+ * @edit Diden89
+ * @version 1.0
+ * @access Public
+ * @path /rab_frontend/scripts/report/report_piutang.js
+ */
+
+const reportPiutang = {
+	selectedData: '',
+	init: function() {
+		const me = this;
+
+		
+		$('#date_range_1').inputmask('dd-mm-yyyy', { 'placeholder': 'DD-MM-YYYY' });
+		$('#date_range_1').noobsdaterangepicker({
+			// parentEl: "#" + popup[0].id + " .modal-body",
+			showDropdowns: true,
+			singleDatePicker: true,
+			locale: {
+				format: 'DD-MM-YYYY'
+			}
+		});
+		$('#date_range_2').inputmask('dd-mm-yyyy', { 'placeholder': 'DD-MM-YYYY' });
+		$('#date_range_2').noobsdaterangepicker({
+			// parentEl: "#" + popup[0].id + " .modal-body",
+			showDropdowns: true,
+			singleDatePicker: true,
+			locale: {
+				format: 'DD-MM-YYYY'
+			}
+		});
+	}
+	
+};
+
+$(document).ready(function() {
+	reportPiutang.init();
+});
